8da1a397c64f933ae536be136aaab99f6105ab71
joey <joey>
2001-01-25 02:04:03 +0000

    r410:    * Added the ability to make debhelper read a different file than      debian/control as the control file. This is very useful for various and      sundry things, all Evil, most involving kernel packages.

--- Debian/Debhelper/Dh_Lib.pm
+++ Debian/Debhelper/Dh_Lib.pm
@@ -383,6 +383,10 @@ sub filearray {
 	}
 }
 
+sub getcontrol {
+	return $ENV{DH_CONTROL} || 'debian/control';
+}
+
 # Returns a list of packages in the control file.
 # Must pass "arch" or "indep" or "same" to specify arch-dependant or
 # -independant or same arch packages. If nothing is specified, returns all
@@ -401,8 +405,8 @@ sub GetPackages {
 	my $package="";
 	my $arch="";
 	my @list=();
-	open (CONTROL,"<debian/control") ||
-		error("cannot read debian/control: $!\n");
+	open (CONTROL, getcontrol()) ||
+		error("cannot read ".getcontrol().": $!\n");
 	while (<CONTROL>) {
 		chomp;
 		s/\s+$//;
