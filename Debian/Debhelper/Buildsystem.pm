95d4393c00bc4b17e115dea95d5fdc6c29cd0b18
Niels Thykier <niels@thykier.net>
2015-05-20 21:52:31 +0200

    D::D::Buildsystem: Preserve the ret val of doit_noerror
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- Debian/Debhelper/Buildsystem.pm
+++ Debian/Debhelper/Buildsystem.pm
@@ -315,16 +315,17 @@ sub doit_in_sourcedir {
 # top directory. Errors are ignored.
 sub doit_in_sourcedir_noerror {
         my $this=shift;
+        my $ret;
         if ($this->get_sourcedir() ne '.') {
                 my $sourcedir = $this->get_sourcedir();
                 $this->_cd($sourcedir);
-                print_and_doit_noerror(@_);
+                $ret = print_and_doit_noerror(@_);
                 $this->_cd($this->_rel2rel($this->{cwd}, $sourcedir));
         }
         else {
-                print_and_doit_noerror(@_);
+                $ret = print_and_doit_noerror(@_);
         }
-        return 1;
+        return $ret;
 }
 
 # Changes working directory to the build directory (if needed),
@@ -349,16 +350,17 @@ sub doit_in_builddir {
 # top directory. Errors are ignored.
 sub doit_in_builddir_noerror {
         my $this=shift;
+        my $ret;
         if ($this->get_buildpath() ne '.') {
                 my $buildpath = $this->get_buildpath();
                 $this->_cd($buildpath);
-                print_and_doit_noerror(@_);
+                $ret = print_and_doit_noerror(@_);
                 $this->_cd($this->_rel2rel($this->{cwd}, $buildpath));
         }
         else {
-                print_and_doit_noerror(@_);
+                $ret = print_and_doit_noerror(@_);
         }
-        return 1;
+        return $ret;
 }
 
 # In case of out of source tree building, whole build directory
