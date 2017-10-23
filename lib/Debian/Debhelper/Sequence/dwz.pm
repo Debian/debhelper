d538205512b9b56389d0979c0eb9614295a92b9b
Niels Thykier <niels@thykier.net>
2017-10-23 06:28:19 +0000

    Add new dh_dwz helper along with a "dwz" sequence
    Signed-off-by: Niels Thykier <niels@thykier.net>

---
+++ lib/Debian/Debhelper/Sequence/dwz.pm
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# Enable dh_dwz
+
+use strict;
+use warnings;
+use Debian::Debhelper::Dh_Lib qw(warning);
+
+warning('This sequence is experimental and may change (or be retired) without any notice');
+
+insert_before('dh_strip', 'dh_dwz');
