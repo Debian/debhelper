f2a5dba91f33baa47e6ff0da7421ddc35e23baab
Niels Thykier <niels@thykier.net>
2017-11-12 14:41:11 +0000

    dwz.pm: Include the name of the sequence in the warning
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/Sequence/dwz.pm
+++ lib/Debian/Debhelper/Sequence/dwz.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use Debian::Debhelper::Dh_Lib qw(warning);
 
-warning('This sequence is experimental and may change (or be retired) without any notice');
+warning('The "dwz"-sequence is experimental and may change (or be retired) without any notice');
 
 insert_before('dh_strip', 'dh_dwz');
 
