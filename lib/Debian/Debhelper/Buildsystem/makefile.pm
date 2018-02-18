f8d7a6ead49e747430124bcea4b55ba05157d4b4
Niels Thykier <niels@thykier.net>
2018-02-18 08:03:42 +0000

    makefile: Pass -O to make with -jX
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/Buildsystem/makefile.pm
+++ lib/Debian/Debhelper/Buildsystem/makefile.pm
@@ -80,7 +80,15 @@ sub do_make {
 	clean_jobserver_makeflags();
 
 	# Note that this will override any -j settings in MAKEFLAGS.
-	unshift @_, "-j" . ($this->get_parallel() > 0 ? $this->get_parallel() : "");
+	my $parallel = $this->get_parallel();
+	if ($parallel == 0 or $parallel > 1) {
+		# We have to use the empty string for "unlimited"
+		$parallel = '' if $parallel == 0;
+		# -O is for synchronizing the output.
+		unshift(@_, "-j${parallel}", '-O');
+	} else {
+		unshift(@_, '-j1');
+	}
 
 	my @root_cmd;
 	if (exists($this->{_run_make_as_root}) and $this->{_run_make_as_root}) {
