59940fb42868be0acf716d2eb29f6c15b59d1daf
Niels Thykier <niels@thykier.net>
2018-03-29 19:10:04 +0000

    Dh_Lib: Avoid load-time dependency on d/control
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/Dh_Lib.pm
+++ lib/Debian/Debhelper/Dh_Lib.pm
@@ -188,6 +188,10 @@ sub init {
 # on, if it's exiting successfully.
 my $write_log=1;
 sub END {
+	# If there is no 'debian/control', then we are not being run from
+	# a package directory and then the write_log will not do what we
+	# expect.
+	return if not -f 'debian/control';
 	if ($? == 0 && $write_log && (compat(9, 1) || $ENV{DH_INTERNAL_OVERRIDE})) {
 		write_log(basename($0), @{$dh{DOPACKAGES}});
 	}
