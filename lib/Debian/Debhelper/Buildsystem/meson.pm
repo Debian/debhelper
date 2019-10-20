3fb30816fc75076ba532f31dae5bcf67125c89fb
Niels Thykier <niels@thykier.net>
2019-10-20 05:07:39 +0000

    meson.pm: Fix broken call to NAME
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/Buildsystem/meson.pm
+++ lib/Debian/Debhelper/Buildsystem/meson.pm
@@ -109,7 +109,7 @@ sub test {
 	my $target = $this->get_targetbuildsystem;
 
 	eval {
-		if (compat(12) or $target->name ne 'ninja') {
+		if (compat(12) or $target->NAME ne 'ninja') {
 			$target->test(@_);
 		} else {
 			# In compat 13 with meson+ninja, we prefer using "meson test"
@@ -118,7 +118,7 @@ sub test {
 				update_env => {
 					'LC_ALL' => 'C.UTF-8',
 				}
-				);
+			);
 			if ($this->get_parallel() > 0) {
 				$options{update_env}{MESON_TESTTHREADS} = $this->get_parallel();
 			}
