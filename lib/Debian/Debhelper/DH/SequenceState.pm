c31843f722a7426b5cf53c912c5adce5a1c95a91
Niels Thykier <niels@thykier.net>
2020-01-26 13:50:03 +0000

    Refactor option state from dh into SequenceState
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/DH/SequenceState.pm
+++ lib/Debian/Debhelper/DH/SequenceState.pm
@@ -17,6 +17,15 @@ our (
 	%obsolete_command,
 	# Commands that can be skipped due to DEB_BUILD_OPTIONS=X flags
 	%commands_skippable_via_deb_build_options,
+	# Options passed that should be passed on to underlying helpers (in order)
+	@options,
+	# Options passed by name (to assist can_skip with which options are used)
+	%seen_options,
+	# Whether there were sequences of options that inhibit certain optimizations
+	# * $unoptimizable_option_bundle => can skip iff cli-options hint is present and empty
+	# * $unoptimizable_user_option => We can never skip anything (non-option seen)
+	$unoptimizable_option_bundle,
+	$unoptimizable_user_option,
 );
 
 1;
