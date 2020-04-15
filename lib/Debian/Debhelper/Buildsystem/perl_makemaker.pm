Debian/debhelper

commit 3cad348f1413931ee789bda75ab3439094964bf5

warn about missing perl-xs-dev for perl_* build systems
When Niko Tyni added cross support to the perl build systems, the
perl_cross_incdir warned about a missing perl-xs-dev. That turned out to
be annoying for a lot of non-perl packages, because dh_perl was emitting
the warning. Therefore, we removed it. However, the warning really is
useful in some cases. Add it back specifically when a perl_* buildsystem
is in use. That should have a better signal/noise ratio.

lib/Debian/Debhelper/Buildsystem/perl_makemaker.pm
@@ -54,7 +54,11 @@ sub configure {
 	my $cross_flag;
 	if (is_cross_compiling()) {
 		my $incdir = perl_cross_incdir();
-		$cross_flag = "-I$incdir" if defined $incdir;
+		if (defined $incdir) {
+			$cross_flag = "-I$incdir";
+		} else {
+			warning("cross Config.pm does not exist (missing build dependency on perl-xs-dev?)");
+		}
 	}
 	if ($ENV{LDFLAGS} && ! compat(8)) {
 		my $ld = $Config{ld};

lib/Debian/Debhelper/Buildsystem/perl_build.pm
@@ -37,6 +37,8 @@ sub do_perl {
 			$perl5lib .= $Config{path_sep} . $ENV{PERL5LIB}
 				if defined $ENV{PERL5LIB};
 			$options{update_env} = { PERL5LIB => $perl5lib };
+		} else {
+			warning("cross Config.pm does not exist (missing build dependency on perl-xs-dev?)");
 		}
 	}
 	$this->doit_in_sourcedir(\%options, "perl", @_);

commit dc629c0f28fec2f0be782b27250807d24f38a1c2

perl_makemaker.pm: missing use of warning
Fixes: 3cad348f1413 ("warn about missing perl-xs-dev for perl_* build systems")
Signed-off-by: Helmut Grohne <helmut@subdivi.de>

lib/Debian/Debhelper/Buildsystem/perl_makemaker.pm
@@ -8,7 +8,7 @@ package Debian::Debhelper::Buildsystem::perl_makemaker;
 
 use strict;
 use warnings;
-use Debian::Debhelper::Dh_Lib qw(compat is_cross_compiling perl_cross_incdir);
+use Debian::Debhelper::Dh_Lib qw(compat is_cross_compiling perl_cross_incdir warning);
 use parent qw(Debian::Debhelper::Buildsystem::makefile);
 use Config;
 