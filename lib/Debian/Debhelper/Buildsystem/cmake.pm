1062f7ee83eea9d4e314777395261241d0aef07f
Paul Wise <pabs@debian.org>
2021-08-31 08:03:27 +0800

    cmake ctest: put arguments before dh_auto_test arguments
    This allows overriding them in debian/rules override_dh_auto_test.

--- lib/Debian/Debhelper/Buildsystem/cmake.pm
+++ lib/Debian/Debhelper/Buildsystem/cmake.pm
@@ -171,8 +171,8 @@ sub test {
 		# -j1). So in order to simulate unlimited parallel, allow to fork a huge
 		# number of threads instead.
 		my $parallel = ($this->get_parallel() > 0) ? $this->get_parallel() : 999;
-		push(@_, "ARGS+=-j$parallel");
-		push(@_, "ARGS+=--verbose") if not get_buildoption("terse");
+		unshift(@_, "ARGS+=-j$parallel");
+		unshift(@_, "ARGS+=--verbose") if not get_buildoption("terse");
 	}
 	return $this->SUPER::test(@_);
 }
