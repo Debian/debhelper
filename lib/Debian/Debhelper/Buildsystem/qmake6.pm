57193a81de3d579a0eab89eae01ca183d5bdc783
Jörg Mechnich <joerg.mechnich@gmail.com>
2024-01-22 06:51:20 +0000

    qmake6.pm: Add cross-compiling support
    Manual merge of Jörg Mechnich's MR!114.
    
    Closes: !114
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/Debian/Debhelper/Buildsystem/qmake6.pm
+++ lib/Debian/Debhelper/Buildsystem/qmake6.pm
@@ -3,12 +3,16 @@ package Debian::Debhelper::Buildsystem::qmake6;
 use strict;
 use warnings;
 use parent qw(Debian::Debhelper::Buildsystem::qmake);
+use Debian::Debhelper::Dh_Lib qw(is_cross_compiling dpkg_architecture_value);
 
 sub DESCRIPTION {
 	"qmake for QT 6 (*.pro)";
 }
 
 sub _qmake {
+	if (is_cross_compiling()) {
+		return dpkg_architecture_value("DEB_HOST_GNU_TYPE") . '-qmake6';
+	}
 	return 'qmake6';
 }
 
